69defcc7e54cdb0e5d6432a43535ce209f087c02
CHIKI2022 <98632445+CHIKI2022@users.noreply.github.com>
2022-01-29 08:36:25 +0330

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+
+pip install pyinstaller
+pip install pyfade
+pip install requests
+pip install selenium
+pip install discord.py
+pip install colored
+pip install discord_webhook
+pip install dhooks
+pip install colorama
+pip install slowprint
+pip install freegames 
+
+pause
+exit
